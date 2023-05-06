950bb4e4ee9b84909df930f13dcdc6521f244960
Chatchai Wongdetsakul <chatchai.wongdet@gmail.com>
2023-05-07 00:17:43 +0700

    Add svelte-adapter-github

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,11 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from '@sveltejs/kit/vite';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from 'svelte-adapter-github';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter()
-	},
-	paths: {
-		base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
-	},
+	}
 };
 
 export default config;
